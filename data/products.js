8cd95603e610f47f396cc760169462aff43f3322
Santiago <sconsoli@itba.edu.ar>
2026-04-23 20:51:32 -0300

    Cambio precios

--- data/products.js
+++ data/products.js
@@ -2,28 +2,28 @@ export const products = [
   {
     id: 'mesa-bmw',
     name: 'Mesa BMW',
-    price: 2400,
+    price: 452000,
     image: '/assets/productos/mesa-bmw.png',
     alt: 'Mesa BMW'
   },
   {
     id: 'reloj-mclaren',
     name: 'Reloj McLaren',
-    price: 1150,
+    price: 150000,
     image: '/assets/productos/reloj-mclaren.png',
     alt: 'Reloj McLaren'
   },
   {
     id: 'porta-cepillos-v10',
     name: 'Porta Cepillos V10',
-    price: 890,
+    price: 50500,
     image: '/assets/productos/porta-cepillos-v10.png',
     alt: 'Porta Cepillos V10'
   },
   {
     id: 'lampara-ciguenal',
     name: 'Lámpara Cigüeñal',
-    price: 1780,
+    price: 68000,
     image: '/assets/productos/lampara-ciguenal.png',
     alt: 'Lámpara Cigüeñal'
   }
